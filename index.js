3b9d4eedec22358261a19be0a13dc08ad2b73308
Saladikhanov <saladikhanov@gmail.com>
2022-01-08 13:35:50 +0200

    Clean and refactored code
    Deleted unused peaces of code and added "try catch"

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// index.js
-
-// require('dotenv').config();
 import dotenv from 'dotenv';
 dotenv.config()
-// import { fetch } from 'node-fetch'
-// import { puppeteerService } from './services/puppeteer.service'
-// import { mustache } from 'mustache'
 import * as fs from 'fs';
 import fetch from 'node-fetch';
 import puppeteerService from './services/puppeteer.service.js';
 import mustache from 'mustache';
-// import { readFile, writeFileSync } from 'fs';
+
 const MUSTACHE_MAIN_DIR = './main.mustache';
 const { render } = mustache;
 
@@ -38,28 +32,46 @@ async function setWeatherInformation() {
     )
       .then(r => r.json())
       .then(r => {
-        DATA.city_temperature = Math.round(r.main.temp);
-        DATA.city_weather = r.weather[0].description;
-        DATA.city_weather_icon = r.weather[0].icon;
-        DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        if(!r) throw new Error("response is undefind");
+        const { weather, main: { temp }, sys: { sunrise, sunset}} = r;
+        const { description, icon } = weather[0];
+     
+        if(!description || !icon || !temp || !sunrise || !sunset) throw new Error("some data is undefind");;
+         
+        const timezonePreset = {
           hour: '2-digit',
           minute: '2-digit',
           timeZone: 'Asia/Jerusalem',
-        });
-        DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
-          hour: '2-digit',
-          minute: '2-digit',
-          timeZone: 'Asia/Jerusalem',
-        });
+        };
+        const sun_rise = new Date(sunrise * 1000).toLocaleString('en-GB', timezonePreset);
+        const sun_set = new Date(sunset * 1000).toLocaleString('en-GB', timezonePreset);
+     
+        const data = {
+         city_temperature: Math.round(temp),
+         city_weather: description,
+         city_weather_icon: icon,
+         sun_rise,
+         sun_set
+        }
+        DATA = { ...DATA, ...data };
+      }).catch(error => {
+        console.log(error.message);
       });
   }
 
 
 async function setInstagramPosts() {
+  try {
     const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('israel', 3);
     DATA.img1 = instagramImages[0];
     DATA.img2 = instagramImages[1];
     DATA.img3 = instagramImages[2];
+
+  } catch (error) {
+    console.log(error.message);
+
+  }
+    
   }
 /**
   * A - We open 'main.mustache'
@@ -67,12 +79,18 @@ async function setInstagramPosts() {
   * C - We create a README.md file with the generated output
   */
 async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
-}
+  try {
+    await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+      if (err) throw err;
+      const output = render(data.toString(), DATA);
+      fs.writeFileSync('README.md', output);
+    });
+
+  } catch (error) {
+    console.log(error.message); 
+  }
+  
+} 
 async function action() {
     /**
      * Fetch Weather
